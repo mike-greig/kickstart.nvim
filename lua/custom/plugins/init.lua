2d92c64f1067895901c7388cf1f9daf52b512f62
mike-greig <MikeGreig@Proton.me>
2025-06-28 15:25:47 -0700

    add toggle term (toggle with <C-s>

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,10 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  { 'akinsho/toggleterm.nvim', opts = {
+    version = '*',
+    config = true,
+    open_mapping = '<c-s>',
+  } },
+}
