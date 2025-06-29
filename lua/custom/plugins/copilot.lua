418bce7ba1c026189650088441237a04d6ef7e79
mike-greig <MikeGreig@Proton.me>
2025-06-29 14:13:10 -0700

    Add copilot chat

---
+++ lua/custom/plugins/copilot.lua
@@ -0,0 +1,19 @@
+return {
+	{ 'github/copilot.vim' }, -- Copilot for Vim
+	{
+		'CopilotC-Nvim/CopilotChat.nvim',
+		dependencies = {
+			{ 'github/copilot.vim' },
+			{ 'nvim-lua/plenary.nvim' },
+		},
+		build = 'make tiktoken',
+		config = function()
+			local select = require 'CopilotChat.select'
+			require('CopilotChat').setup {
+				selection = function(source)
+					return select.visual(source) or select.buffer(source)
+				end,
+			}
+		end,
+	},
+}
